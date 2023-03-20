cf0ae774d99b57c1ecdd2e600f1fe0c40360357a
Alejo27052022 <stalinalejandro19@outlook.es>
2023-03-20 00:06:39 -0500

    Terminacion del Desktop

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel_lista'), {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.carousel_indicadores',
+
+        responsive: [
+            {
+              breakpoint: 775,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                itemWidth: 150,
+                duration: 0.25
+              }
+            },{
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            }
+          ]
+    });
+});
