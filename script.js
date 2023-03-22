6b3c4af25ae922f4d4da9db02a6ac8fbb617778b
Alejo27052022 <stalinalejandro19@outlook.es>
2023-03-22 01:51:49 -0500

    Terminacion del Projecto Responsive

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const IconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+//Eventos de Click
+IconMenu.addEventListener('click', toggleMobileMenu);
+
 window.addEventListener('load', function(){
     new Glider(document.querySelector('.carousel_lista'), {
         slidesToShow: 3,
@@ -6,22 +12,46 @@ window.addEventListener('load', function(){
         dots: '.carousel_indicadores',
 
         responsive: [
-            {
-              breakpoint: 775,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                itemWidth: 150,
-                duration: 0.25
-              }
-            },{
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-              }
+          {
+            breakpoint: 300,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
             }
-          ]
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 700,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            }
+          }
+        ]
     });
 });
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
+}
 
